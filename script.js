5c8b460cd08d1288e16cac5b098b8b6702be2393
Adam Grover <adamggrover@gmail.com>
2022-05-05 20:25:04 +0100

    write functions

---
+++ script.js
@@ -0,0 +1,18 @@
+function add7(a){
+    return a+7;
+    
+    }
+
+function multiply(a,b){
+    return a*b;
+}
+function capitalize(string){
+    return string.charAt(0).toUpperCase()+string.toLowerCase().substring(1);
+}
+
+function lastLetter(string){
+    return string.charAt(string.length - 1)
+
+}
+
+
